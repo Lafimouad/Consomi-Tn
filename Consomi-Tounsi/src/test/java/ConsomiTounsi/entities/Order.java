2048b3869319f55edaed05d970409c3c5e21a342
Lafimouad <mouad19971@gmail.com>
2021-03-09 00:34:47 +0100

    MOUADH

---
+++ Consomi-Tounsi/src/test/java/ConsomiTounsi/entities/Order.java
@@ -0,0 +1,32 @@
+package ConsomiTounsi.entities;
+
+import java.io.Serializable;
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+@Entity
+public class Order implements Serializable {
+	
+	@Id
+	@GeneratedValue( strategy = GenerationType.IDENTITY)
+	private int id_order;
+	@Temporal (TemporalType.DATE)
+	private Date date_order;
+	
+	@Enumerated(EnumType.STRING)
+	private Payment paymentType_order;
+	
+	private float cost_order;
+	private boolean paid_order;
+	private float weight_order;
+	
+
+}
