6deecc08694e5688f784af91dd3592318f27dbed
Lafimouad <mouad19971@gmail.com>
2021-03-09 03:13:11 +0100

    mouadh

--- Consomi-Tounsi/src/main/java/ConsomiTounsi/entities/Order.java
+++ Consomi-Tounsi/src/main/java/ConsomiTounsi/entities/Order.java
@@ -9,10 +9,13 @@ import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 
 @Entity
+@Table(name="\"Order\"")
+
 public class Order implements Serializable {
 	
 	@Id
